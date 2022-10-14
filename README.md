1a2cc1ede2c1bb42e6cd6b840167e757b43e97a0
d8texe <d8texe@gmail.com>
2022-10-14 11:33:14 +0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # stormworks-borderless
 Make game window borderless fullscreen.
+
+1. Open the game.
+2. Set windowed mode in options.
+3. Run stormless.exe
+4. PROFIT!
---
+++ stormless.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+    HWND hWin;
+
+    hWin = FindWindow(NULL, L"Stormworks");
+
+    if(hWin == NULL)
+    {
+        std::cout << "Stormworks not found!" << std::endl;
+        getchar();
+        return 1;
+    }
+
+    int w = GetSystemMetrics(SM_CXSCREEN);
+    int h = GetSystemMetrics(SM_CYSCREEN);
+    SetWindowLongPtr(hWin, GWL_STYLE, WS_VISIBLE | WS_OVERLAPPED);
+    SetWindowPos(hWin, HWND_TOP, 0, 0, w, h, SWP_FRAMECHANGED);
+    ShowWindow(hWin, SW_MAXIMIZE);
+}
